9e2234e75fbdf55e91b67c9b14fa9af0e2f8aa71
Tomislav Cicek <tomii.mf@gmail.com>
2024-05-21 20:12:48 +0200

    rsdhs

---
+++ script.js
@@ -0,0 +1,16 @@
+const boja = "zuta";
+
+switch(boja){
+    case "zuta":
+        console.log("odabrao si zutu boju");
+        break;
+        case "crvena":
+            console.log("odabrao si crvenu boju");
+            break;
+            case "zelena":
+                console.log("odabrao si zelenu boju");
+                break;
+
+                default: 
+                console.log("ne znam koju si boju odabrao")
+}
